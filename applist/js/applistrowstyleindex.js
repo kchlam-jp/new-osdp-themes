960123e772c677fef8ba1d538ee2a08f48ff5e65
kchlam-jp <57714600+kchlam-jp@users.noreply.github.com>
2020-05-07 17:42:46 +0800

    07-may-2020

---
+++ applist/js/applistrowstyleindex.js
@@ -0,0 +1,16 @@
+var appListDefaultRowStyle = 
+	"<tr class='applistRow'>"  +
+	"<td class='appProvider'>#appProvider#</td>" +
+	"<td class='appName'>#appUrl#</td>" +
+	"</tr>";
+
+var appListRowStyle = {
+	"OSDP12 Application 6 - HKID" : "<tr class='applistRow'>"  +
+		"<td class='appProvider' bgcolor='#FF0000'>#appProvider#</td>" +
+		"<td class='appName'>#appUrl#</td>" +
+		"</tr>",
+	"OSDP12 Application 8" : "<tr class='applistRow'>"  +
+		"<td class='appProvider' bgcolor='#0000FF'>#appProvider#</td>" +
+		"<td class='appName' bgcolor='#00FF00'>#appUrl#</td>" +
+		"</tr>"
+};
